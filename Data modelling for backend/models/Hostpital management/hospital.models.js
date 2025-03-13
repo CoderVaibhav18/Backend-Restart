4fb1b297c02412ef4cce79598b365ee3293f75fa
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-03-13 22:44:45 +0530

    Hospital management schema done

---
+++ Data modelling for backend/models/Hostpital management/hospital.models.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const hospitaSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    pincode: {
+      type: Number,
+      required: true,
+    },
+    specializedIn: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
