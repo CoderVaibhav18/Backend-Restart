fa26b29af1f04e1ec249da71132ea107ba3993ef
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-03-16 18:03:04 +0530

    monooge aggregate pipplines

--- professional setup for node/src/models/user.model.js
+++ professional setup for node/src/models/user.model.js
@@ -1,54 +1,54 @@
 import mongoose, { mongo } from "mongoose";
 
 const userSchema = new mongoose.Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-            trim: true,
-            index: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-            trim: true,
-        },
-        fullname: {
-            type: String,
-            required: true,
-            index: true,
-            trim: true,
-        },
-        avatar: {
-            type: String,
-            required: true,
-        },
-        coverImage: {
-            type: String,
-        },
-        watchHistory: {
-            type: [
-                {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "Video",
-                },
-            ],
-        },
-        password: {
-            type: String,
-            required: [true, "password is required"],
-        },
-        refreshToken: {
-            type: String,
-        },
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
-    {
-        timestamps: true, // createdAt and updatedAt
-    }
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      index: true,
+      trim: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Video",
+        },
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true, // createdAt and updatedAt
+  }
 );
 
 export const User = mongoose.model("User", userSchema);
