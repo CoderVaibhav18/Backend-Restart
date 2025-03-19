6da5b3c00b1161aafa23c7d44bd76a766e77be35
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-03-20 00:55:01 +0530

    user route created

--- professional setup for node/src/app.js
+++ professional setup for node/src/app.js
@@ -4,10 +4,10 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(
-    cors({
-        origin: "*",
-        credentials: true,
-    })
+  cors({
+    origin: "*",
+    credentials: true,
+  })
 );
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
@@ -15,7 +15,13 @@ app.use(cookieParser());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
+
+// import routes
+import userRouter from "./routes/user.route.js";
+
+// user routes declear
+app.use("/api/v1/users", userRouter);
 
 export default app;
--- professional setup for node/src/utils/asyncHandler.js
+++ professional setup for node/src/utils/asyncHandler.js
@@ -1,11 +1,8 @@
-export const asyncHandler = (requestHandler) => async (req, res, next) => {
-    (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) =>
-            next(err)
-        );
-    };
+export const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
 };
-
 
 // export const asyncHandler = (fn) => async (req, res, next) => {
 //     try {
