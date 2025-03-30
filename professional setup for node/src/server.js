b39cf7052a4c5dffffae9d3dbe513f578e432fc7
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-03-30 13:09:35 +0530

    d

--- professional setup for node/src/server.js
+++ professional setup for node/src/server.js
@@ -1,20 +1,20 @@
 import dotenv from "dotenv";
 dotenv.config({
-    path: "./.env",
+  path: "./.env",
 });
 import { connectDB } from "./db/db.js";
 import app from "./app.js";
 const PORT = process.env.PORT || 6000;
 
 connectDB()
-    .then(() => {
-        app.on("error", (err) => {
-            console.log("Error:", err);
-        });
-        app.listen(PORT, () => {
-            console.log(`Server running at port: ${PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.log("MongoDB connection error !!! ", err);
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Error:", err);
     });
+    app.listen(PORT, () => {
+      console.log(`Server running at port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error !!! ", err);
+  });
